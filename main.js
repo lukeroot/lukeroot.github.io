d08effef13d4851166aaf5ff55bccf6bf95561e4
lukeroot <lukeroot@pm.me>
2025-03-26 12:58:32 +0000

    polyfill

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 $(() => {
+    Array.prototype.myMap = function(callbackFn) {
+      var arr = [];
+      for (var i = 0; i < this.length; i++) {
+         /* call the callback function for every value of this array and       push the returned value into our resulting array
+         */
+        arr.push(callbackFn(this[i], i, this));
+      }
+      return arr;
+    }
     class Random
     {
         static cyrb128(str)
